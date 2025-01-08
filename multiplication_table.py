a75de984dbd3059961dd8a1681d63209db6c1b55
ABUBAKAR MUHAMMAD DANHAYA <121393884+HABUDANHAYABK@users.noreply.github.com>
2025-01-08 23:47:48 +0100

    added reversed strings

--- multiplication_table.py
+++ multiplication_table.py
@@ -3,3 +3,9 @@
 num = int(input("Enter a number:"))
 for i in range(1,11):
     print(f"{num} x {i} = {num * i}")
+
+    # Reverse Strings
+
+    string = input("Enter a string:")
+    reversed_string = string[::-1]
+    print("Reversed string:", reversed_string)
